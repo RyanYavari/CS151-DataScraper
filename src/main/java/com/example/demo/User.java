3047f7fb8f068d52635ac018684f55f760bd281c
Shirley Li <shirleyli456@gmail.com>
2022-11-23 13:28:59 -0800

    Fixed error message

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,62 @@
+package com.example.demo;
+import javax.swing.JPasswordField;
+
+public class User {
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password; // ehe
+	private String username;
+	
+	public User(String firstName, String lastName, String email, String password, String username) {
+		this.setFirstName(firstName);
+		this.setLastName(lastName);
+		this.setEmail(email);
+		this.setPassword(password);
+		this.setUsername(username);
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
+
+}
