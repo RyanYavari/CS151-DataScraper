9cb2f518e92fe062d508cf80cd3d7b7fe2b46c95
shhhushhh <shirleyli456@gmail.com>
2022-11-23 21:02:42 -0800

    More ui updates

--- src/main/java/com/example/demo/Cs151DataScraperApplication.java
+++ src/main/java/com/example/demo/Cs151DataScraperApplication.java
@@ -27,4 +27,58 @@ public class Cs151DataScraperApplication {
 		return "Authentication not successful :(";
 	}
 
+	@CrossOrigin
+	@GetMapping("/checkValidUser")
+	public String checkValidUser(@RequestParam (name = "firstName", defaultValue = "") String firstName,
+		@RequestParam (name = "lastName", defaultValue = "") String lastName,
+		@RequestParam (name = "email", defaultValue = "") String email,
+		@RequestParam (name = "password", defaultValue = "") String password) {
+		// currently this is just some dummy code
+		// should be replaced by accessing database and checking there
+		if (email.equals("dfs") && password.equals("dfs")) {
+			return "New user created!";
+		}
+		return "New user was not created :(";
+	}
+
+	// private void verifyPassword(char[] password) throws PasswordException {
+	// 	int upperCase = 0;
+	// 	int lowerCase = 0;
+	// 	Pattern regex = Pattern.compile("[$&+,:;=\\\\?@#|/'<>.^*()%!-]");
+	// 	int numChar = 0;
+		
+	// 	if (password.length < 8) {
+	// 		throw new Minimum8CharactersRequired();
+	// 	}
+		
+	// 	for (int i = 0; i < password.length; i++) {
+	// 		if (Character.isUpperCase(password[i])) {
+	// 			upperCase++;
+	// 		} else if (Character.isLowerCase(password[i])) {
+	// 			lowerCase++;
+	// 		} else if (Character.isDigit(password[i])) {
+	// 			numChar++;
+	// 		} 
+	// 	}
+		
+	// 	if (upperCase == 0) {
+	// 		throw new UpperCaseCharacterMissing();
+	// 	}
+		
+	// 	if (lowerCase == 0) {
+	// 		throw new LowerCaseCharacterMissing();
+	// 	}
+		
+	// 	if (numChar == 0) {
+	// 		throw new NumberCharacterMissing();
+	// 	}
+		
+	// 	StringBuilder s = new StringBuilder();
+	// 	s.append(password);
+	// 	if (!regex.matcher(s).find()) {
+	// 		throw new SpecialCharacterMissing();
+	// 	}
+	
+	// }
+
 }
