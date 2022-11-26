1b646b718e3e87bdd89b65ea245fcbb8cc12190f
RyanYavari <48726914+RyanYavari@users.noreply.github.com>
2022-11-26 01:03:22 -0800

    Added parser and formatter

---
+++ bin/datascraperfrontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import Login from "./components/Login";
+
+function App() {
+  // useEffect( () => {
+  //   fetch('http://localhost:8080/hello').then(response=>response.text).then(result=>console.log(result));
+  // }, []);
+
+
+  return (
+    <Login />
+  );
+}
+
+export default App;
---
+++ bin/datascraperfrontend/src/components/Login.js
@@ -0,0 +1,167 @@
+import * as React from "react";
+import { useRef } from "react";
+import TextField from "@mui/material/TextField";
+import { Button, Hidden } from "@mui/material";
+import { ClassNames } from "@emotion/react";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem"; 
+
+export default function Login() {
+  const email = useRef(null);
+  const password = useRef(null);
+
+  function authenticateUser() {
+    console.log(email.current.value);
+    console.log(password.current.value);
+    fetch(
+      "http://localhost:8080/authenticate?email=" +
+        email.current.value +
+        "&password=" +
+        password.current.value
+    )
+      .then((response) => response.text)
+      .then((result) => console.log(result));
+  }
+
+  function setSignUpComponent() {
+    
+  }
+
+  return (
+    <Box
+      component="form"
+      sx={{ 
+        width: "100%",  
+        height: "100%",
+        mx: "auto", 
+        display: 'flex',
+        justifyContent: 'center'
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <List
+          sx={{
+            mx: "auto",
+            width: 500,
+            mt: 20,
+            mb: 20,
+            bgcolor: (theme) =>
+              theme.palette.mode === "dark" ? "#101010" : "grey.50",
+            color: (theme) =>
+              theme.palette.mode === "dark" ? "grey.300" : "grey.800",
+            border: "1px solid",
+            borderColor: (theme) =>
+              theme.palette.mode === "dark" ? "grey.800" : "grey.300",
+            borderRadius: 2,
+            textAlign: "center",
+            fontSize: "0.875rem",
+            fontWeight: "700",
+          }}
+        >
+          <ListItem
+            sx={{
+              mx: "auto",
+              width: 300,
+              justifyContent: "center",
+              textAlign: "center",
+              fontSize: "1.875rem",
+              fontWeight: "700"
+            }}
+            >
+              <label>DataScraper</label>
+          </ListItem>
+          <ListItem
+            sx={{
+              mx: "auto",
+              width: 300,
+            }}
+          >
+            <TextField
+              sx={{
+                bgcolor: (theme) =>
+                  theme.palette.mode === "light" ? "#FFFFFF" : "white",
+                width: 300,
+              }}
+              id="emailInput"
+              label="Email"
+              autoComplete="current-email"
+              inputRef={email}
+            />
+          </ListItem>
+          <ListItem
+            sx={{
+              mx: "auto",
+              width: 300,
+            }}
+          >
+            <TextField
+              sx={{
+                bgcolor: (theme) =>
+                  theme.palette.mode === "light" ? "#FFFFFF" : "white",
+                width: 300,
+              }}
+              id="passwordInput"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              inputRef={password}
+            />
+          </ListItem>
+          <ListItem
+            sx={{
+              mx: "auto",
+              width: 300,
+            }}
+          >
+            <Button
+              variant="outlined"
+              onClick={authenticateUser}
+              sx={{
+                bgcolor: (theme) =>
+                  theme.palette.mode === "light" ? "#FFFFFF" : "white",
+                mx: "auto",
+                width: 105,
+              }}
+            >
+              Login
+            </Button>
+          </ListItem>
+          <ListItem
+            sx={{
+              mx: "auto",
+              width: 300,
+              justifyContent: "center",
+              textAlign: "center",
+              fontSize: "0.875rem",
+              fontWeight: "200"
+            }}
+            >
+              <label>OR</label>
+          </ListItem>
+          <ListItem
+            sx={{
+              mx: "auto",
+              width: 300,
+            }}
+          >
+            <Button
+              variant="outlined"
+              onClick={setSignUpComponent}
+              sx={{
+                bgcolor: (theme) =>
+                  theme.palette.mode === "light" ? "#FFFFFF" : "white",
+                mx: "auto",
+                width: 105,
+              }}
+            >
+              Signup
+            </Button>
+          </ListItem>
+        </List>
+      </div>
+    </Box>
+  );
+}
