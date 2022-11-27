548183996b7fdc8afe39e2196ae268982ed3cb98
Ryan Yavari <48726914+RyanYavari@users.noreply.github.com>
2022-11-27 11:43:09 -0800

    Finalized Formatter file

--- src/main/java/com/example/demo/Formatter.java
+++ src/main/java/com/example/demo/Formatter.java
@@ -2,55 +2,108 @@ package com.example.demo;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+/*
+ * This class stores information in the userInfo HashMap by taking an input from the Parser class.
+ * The userInfo:
+ * 		Key: Twitter Handle
+ * 		Value: Link to tweet containing keyword 
+ * 
+ * In the main method, a Parser object is created that intakes (query,max_results): Ex: ("ostrich,egg", 20)
+ * 		Restrictions to Parser: 
+ * 				Maximum query is 512 characters
+ * 				Cannot contain white spaces, keywords must be separated by commas. Example: "ostrich egg" won't work. 
+ * 				max_results can only accept values between 10 and 100. 
+ * 								
+ */
+
 public class Formatter {
+
+	
+	private StringBuilder unformatted;
+	private Map<String, String> userInfo;
 	
 
-	private StringBuilder unformatted;
-	//private Map<String, String> ids;
-	private ArrayList<String> ids;
-	private ArrayList<String> handles;
-	
 	public Formatter(String unformat) {
 		unformatted = new StringBuilder(unformat);
-		//ids = new HashMap<>();
-		ids = new ArrayList<>();
-		handles = new ArrayList<>();
-	}
-	
-	
-	public void formatID() {
-	
-		int startPoint;
-		while(unformatted.toString().contains("\"id\"")) {
-			startPoint = unformatted.lastIndexOf("\"id\"");
-			ids.add(unformatted.substring(startPoint+6, startPoint+24));
-			unformatted.delete(startPoint, startPoint+27);
-		}
-		System.out.println(ids);
+		userInfo = new HashMap<>();
 		
 	}
-	
-	public void formatHandle() {
-		int startPoint;
-		while(unformatted.toString().contains("\"id\"")) {
-			startPoint = unformatted.lastIndexOf("\"id\"");
-			ids.add(unformatted.substring(startPoint+6, startPoint+24));
-			unformatted.delete(startPoint, startPoint+27);
-		}
-		System.out.println(ids);
+
+	public void format() throws JsonMappingException, JsonProcessingException, JSONException, UnirestException {
+
+		JSONArray array = new JSONArray(unformatted.toString().substring(8));  
+		String tempPlacer;
+		String currentUser;
+		for(int i=0; i < array.length(); i++)   
+		{  
+			JSONObject object = array.getJSONObject(i); 
+			
+			
+			
+			
+			tempPlacer = Parser.searchUsername(object.getString("author_id"));
+			currentUser = tempPlacer.substring(tempPlacer.indexOf("\"username\":")+12, tempPlacer.length()-3);
+			
+			userInfo.put(currentUser, formURL(currentUser,object.getString("id")));
+			
+			
+		}  
+		
+	}  
+
+	public String formURL(String username, String tweetID) {
+		return ("https://twitter.com/" + username + "/status/" + tweetID);
+	}
+
+	public StringBuilder getUnformatted() {
+		return unformatted;
+	}
+
+	public void setUnformatted(StringBuilder unformatted) {
+		this.unformatted = unformatted;
 	}
 	
-	public static void main(String[] args) throws IOException, URISyntaxException {
-		Parser data = new Parser("dog cat");
+	public void print() {
+		for(String username: userInfo.keySet()) {
+			System.out.println("Handle:" + username + ", Tweet URL: " + userInfo.get(username));
+		}
+	}
+
+	public static void main(String[] args) throws IOException, URISyntaxException, UnirestException {
+		Parser data = new Parser("Ostrich,egg", 20);
 		Formatter formattedData = new Formatter(data.getOutput());
+
 		data.print();
 		
-		formattedData.formatID();
+		System.out.println();
+
+		formattedData.format();
+		
+		formattedData.print();
+
+		System.out.println();
+		System.out.println();
+		System.out.println();
 	}
-	
+
+	public Map<String, String> getUserInfo() {
+		return userInfo;
+	}
+
+	public void setUserInfo(Map<String, String> userInfo) {
+		this.userInfo = userInfo;
+	}
+
 
 }
